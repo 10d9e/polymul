9270c7c242b04c05b42b2a2415d8bfafa5c751e2
lodge <jay.logelin@gmail.com>
2026-06-24 14:55:52 -0300

    CRT: drop redundant reduction of r0 residue mod P1

--- src/algorithm/mod.rs
+++ src/algorithm/mod.rs
@@ -402,8 +402,8 @@ pub fn poly_mul(plan: &mut Plan, a: &[u32; 1024], b: &[u32; 1024]) -> [u32; 1024
     unsafe {
         for j in 0..N {
             let v0 = *r0.get_unchecked(j);
-            // v1 = (r1 - v0) * inv(P0) mod P1
-            let t1 = (*r1.get_unchecked(j) + p1 - v0 % p1) % p1;
+            // v1 = (r1 - v0) * inv(P0) mod P1.  v0 = r0[j] < P0 < P1, so v0 % P1 == v0.
+            let t1 = (*r1.get_unchecked(j) + p1 - v0) % p1;
             let v1 = (t1 * inv01) % p1;
             // w = (v0 + P0*v1) mod P2
             let w = (v0 % p2 + p0_mod_p2 * v1 % p2) % p2;
